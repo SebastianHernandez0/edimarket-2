03edd38be5981f78acada18571164ec6b014ed1d
Edison <aalevenegass@gmail.com>
2024-06-10 22:45:20 -0400

    Estilando y agregando íconos en navbar

--- Frontend/src/components/navbar/Navbar.jsx
+++ Frontend/src/components/navbar/Navbar.jsx
@@ -1,11 +1,32 @@
 import "../navbar/navbar.css";
+import { FiSearch } from "react-icons/fi";
+import { FaUserCircle } from "react-icons/fa";
+import { CgMenuRightAlt } from "react-icons/cg";
+import { IoClose } from "react-icons/io5";
 
 export function Navbar() {
   return (
-    <nav className="navbar__container bg-blue-900 h-16">
-      <h1 className="navbar__title text-white text-center">
-        Hola soy el navbar
-      </h1>
+    <nav className="navbar__container shadow-sm">
+      <div className="navbar__logo__container">
+        <img
+          className="navbar__logo__img"
+          src="./imgs/aplication/navLogo2.1.png"
+          alt=""
+        />
+        <div className="navbar__search__container">
+          <input className="navbar__search__input" type="text" />
+          <FiSearch className="navbar__search__icon" />
+        </div>
+        <div className="navbar__user__container">
+          <div className="navbar__user__icons__container">
+            <FaUserCircle className="navbar__user__icon" />
+            <div className="navbar__icons__menu__container">
+              <CgMenuRightAlt className="navbar__user__icon navbar__user__icon__open" />
+              <IoClose className="navbar__user__icon navbar__user__icon__close" />
+            </div>
+          </div>
+        </div>
+      </div>
     </nav>
   );
 }
