d5b9369d19ef521d83d4d500d6f7d1ae28e87668
Edison <aalevenegass@gmail.com>
2024-06-11 14:27:39 -0400

    Creando vista para crear una cuenta

---
+++ Frontend/src/components/perfilBtn/PerfilBtn.jsx
@@ -0,0 +1,7 @@
+export function PerfilBtn({ className, children }) {
+  return (
+    <div className={className}>
+      <button className={className}>{children}</button>
+    </div>
+  );
+}
--- Frontend/src/pages/singUp/singUp.jsx
+++ Frontend/src/pages/singUp/singUp.jsx
@@ -1,9 +1,47 @@
+import { NavLink } from "react-router-dom";
+import { PerfilBtn } from "../../components/perfilBtn/PerfilBtn";
 import "./singUp.css";
 
 export function SingUp() {
   return (
     <section className="register__container">
-      <h1 className="register__title">Hola soy el register</h1>
+      <div className="register__form__container">
+        <form className="register__form">
+          <div className="register__form__title__container">
+            <h1 className="register__form__title text-center text-2xl font-medium">
+              Bienvenid@s a EdiMarket
+            </h1>
+            <p className="register__form__paragraph text-center text-sm my-2">
+              Por favor ingresa tus datos para crear tu cuenta
+            </p>
+          </div>
+          <div className="register__form__input__container">
+            <div className="register__input__container">
+              <input className="register__form__input" type="text" />
+              <p className="register__form__input__paragraph">Email</p>
+            </div>
+            <div className="register__input__container">
+              <input className="register__form__input" type="password" />
+              <p className="register__form__input__paragraph">Contraseña</p>
+            </div>
+            <div className="register__input__container">
+              <input className="register__form__input" type="password" />
+              <p className="register__form__input__paragraph">
+                Confirmar Contraseña
+              </p>
+            </div>
+          </div>
+          <PerfilBtn className="bg-teal-300">CREAR CUENTA</PerfilBtn>
+          <div className="register__form__singin">
+            <p className="register__form__paragraph text-sm">
+              ¿Ya tienes cuenta?
+            </p>
+            <NavLink className="register__form__link text-sm">
+              Iniciar Sesión
+            </NavLink>
+          </div>
+        </form>
+      </div>
     </section>
   );
 }
