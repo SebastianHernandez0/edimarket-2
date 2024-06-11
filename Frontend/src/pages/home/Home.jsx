29823d73166a798b2cdc3e10dec3a578f71895d5
Edison <aalevenegass@gmail.com>
2024-06-11 19:34:46 -0400

    Creando archivo json para productos (prueba de maqueo y creación de cards en home)

--- Frontend/src/pages/home/Home.jsx
+++ Frontend/src/pages/home/Home.jsx
@@ -4,11 +4,8 @@ import "../home/home.css";
 export function Home() {
   return (
     <section className="home__container">
-      <div className="display: flex items-center gap-1">
-        <FaHome className="home__icon text-3xl text-blue-600" />
-        <h1 className="home__title font-medium text-red-600 text-3xl">
-          Hola soy el home
-        </h1>
+      <div className="home__cards__container">
+
       </div>
     </section>
   );
