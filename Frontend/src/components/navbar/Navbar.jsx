23883b4a2a4e915a84e870adfc48b87457ec63f3
Edison <aalevenegass@gmail.com>
2024-06-10 23:06:46 -0400

    Creando estado para controlar la búsqueda en mobile first

--- Frontend/src/components/navbar/Navbar.jsx
+++ Frontend/src/components/navbar/Navbar.jsx
@@ -3,8 +3,25 @@ import { FiSearch } from "react-icons/fi";
 import { FaUserCircle } from "react-icons/fa";
 import { CgMenuRightAlt } from "react-icons/cg";
 import { IoClose } from "react-icons/io5";
+import { useEffect, useState } from "react";
 
 export function Navbar() {
+  const [openSearchBar, setOpenSearchBar] = useState(false);
+
+  const handleOpenSearchBar = () => {
+    setOpenSearchBar(!openSearchBar);
+  };
+
+  useEffect(() => {
+    const searchBar = document.querySelector(".navbar__search__input");
+
+    if (openSearchBar) {
+      searchBar.classList.add("searchInputActive");
+    } else {
+      searchBar.classList.remove("searchInputActive");
+    }
+  }, [openSearchBar]);
+
   return (
     <nav className="navbar__container shadow-sm">
       <div className="navbar__logo__container">
@@ -14,8 +31,15 @@ export function Navbar() {
           alt=""
         />
         <div className="navbar__search__container">
-          <input className="navbar__search__input" type="text" />
-          <FiSearch className="navbar__search__icon" />
+          <input
+            placeholder="Busca un producto"
+            className="navbar__search__input"
+            type="text"
+          />
+          <FiSearch
+            onClick={handleOpenSearchBar}
+            className="navbar__search__icon"
+          />
         </div>
         <div className="navbar__user__container">
           <div className="navbar__user__icons__container">
