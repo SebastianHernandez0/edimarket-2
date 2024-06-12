ce2781943c365306b5dc61ca1e529cce41ea101c
Edison <aalevenegass@gmail.com>
2024-06-11 21:05:39 -0400

    Añadiendo efecto hover en cards

--- Frontend/src/components/products/Products.jsx
+++ Frontend/src/components/products/Products.jsx
@@ -1,22 +1,29 @@
 import { ProductCard } from "../../components/productCard/ProductCard.jsx";
 import products from "/public/product.json";
+import "../products/products.css";
 
 export function Products() {
   return (
     <div className="products__container">
-      <div className="home__cards__container">
+      <h1 className="products__title text-2xl font-semibold">
+        Productos recomendados
+      </h1>
+      <div className="products__cards__container">
         {products.map((product) => (
-          <ProductCard key={product.id} className="home__card shadow-md">
+          <ProductCard
+            key={product.id}
+            className="products__card shadow-md bg-white"
+          >
             <img
-              className="home__card__img"
+              className="products__card__img"
               src={product.href}
               alt={product.nombre}
             />
-            <div className="home__card__desc__container px-4">
-              <p className="home__card__paragraph text-slate-700 font-semibold text-lg">
+            <div className="products__card__desc__container px-4">
+              <p className="products__card__paragraph text-slate-700 font-semibold text-lg">
                 {product.nombre}
               </p>
-              <p className="home__card__paragraph font-semibold text-2xl">
+              <p className="products__card__paragraph font-semibold text-2xl">
                 {product.precio.toLocaleString("es-CL", {
                   style: "currency",
                   currency: "CLP",
