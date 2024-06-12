a9320da0167c8028586b48ac4206a4ef719bce70
Edison <aalevenegass@gmail.com>
2024-06-11 20:27:55 -0400

    Termiminando estilos generales del componente card

--- Frontend/src/pages/home/Home.jsx
+++ Frontend/src/pages/home/Home.jsx
@@ -2,25 +2,28 @@ import "../home/home.css";
 import products from "../../../public/product.json";
 import { ProductCard } from "../../components/productCard/ProductCard";
 
-console.log(products);
 export function Home() {
   return (
     <section className="home__container">
       <div className="home__cards__container">
         {products.map((product) => (
-          <ProductCard key={product.id} className="home__card">
+          <ProductCard key={product.id} className="home__card shadow-md">
             <img
               className="home__card__img"
               src={product.href}
               alt={product.nombre}
             />
-            <p className="home__card__paragraph">{product.nombre}</p>
-            <p className="home__card__paragraph">
-              {product.precio.toLocaleString("es-CL", {
-                style: "currency",
-                currency: "CLP",
-              })}
-            </p>
+            <div className="home__card__desc__container px-4">
+              <p className="home__card__paragraph text-slate-700 font-semibold text-lg">
+                {product.nombre}
+              </p>
+              <p className="home__card__paragraph font-semibold text-xl">
+                {product.precio.toLocaleString("es-CL", {
+                  style: "currency",
+                  currency: "CLP",
+                })}
+              </p>
+            </div>
           </ProductCard>
         ))}
       </div>
