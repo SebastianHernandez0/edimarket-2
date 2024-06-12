8884f128f190d56a0e41d9b0660ea5ea91ab896b
Edison <aalevenegass@gmail.com>
2024-06-12 01:09:17 -0400

    Ajustando estilos de card vista detalle

--- Frontend/src/pages/productDetail/ProductDetail.jsx
+++ Frontend/src/pages/productDetail/ProductDetail.jsx
@@ -11,28 +11,33 @@ export function ProductDetail() {
     <section className="productdetail__container">
       <div className="card__container">
         <ProductCard className="card__body shadow-md rounded-md">
-          <img src={productById?.href} alt="" />
-          <div className="card__info">
-            <p className="card__paragraph card__paragraph__name">
-              {productById?.nombre}
-            </p>
-            <p className="card__paragraph card__paragraph__price">
-              {productById?.precio.toLocaleString("es-CL", {
-                style: "currency",
-                currency: "CLP",
-              })}
-            </p>
-            <p className="card__paragraph card__paragraph__stock">
-              Stock disponible{" "}
-              <span className="font-semibold">{productById?.stock}</span>
-            </p>
+          <img className="card__img" src={productById?.href} alt="" />
+          <div className="card__info border-2 rounded-md">
+            <div className="card__info__details">
+              <p className="card__paragraph card__paragraph__name">
+                {productById?.nombre}
+              </p>
+              <p className="card__paragraph card__paragraph__price">
+                {productById?.precio.toLocaleString("es-CL", {
+                  style: "currency",
+                  currency: "CLP",
+                })}
+              </p>
+              <p className="card__paragraph card__paragraph__stock">
+                Stock disponible
+                <span className="font-semibold">{productById?.stock}</span>
+              </p>
+            </div>
+
             <div className="card__info__btn__container">
-              <GeneralBtn className="card__info__btn card__info__btn__buy">Comprar ahora</GeneralBtn>
+              <GeneralBtn className="card__info__btn card__info__btn__buy">
+                Comprar ahora
+              </GeneralBtn>
               <GeneralBtn className="card__info__btn card__info__btn__cart">
                 Agregar al carrito
               </GeneralBtn>
             </div>
-            <hr className="mt-8"/>
+            <hr className="mt-8" />
           </div>
           <div className="card__info__desc">{productById?.descripcion}</div>
         </ProductCard>
