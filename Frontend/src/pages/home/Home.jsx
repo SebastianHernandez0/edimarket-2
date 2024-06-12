f3088edac93c615fec1231f1b800042dccf00aa7
Edison <aalevenegass@gmail.com>
2024-06-11 20:03:33 -0400

    Mapeando resultados del json

--- Frontend/src/pages/home/Home.jsx
+++ Frontend/src/pages/home/Home.jsx
@@ -1,11 +1,28 @@
-import { FaHome } from "react-icons/fa";
 import "../home/home.css";
+import products from "../../../public/product.json";
+import { ProductCard } from "../../components/productCard/ProductCard";
 
+console.log(products);
 export function Home() {
   return (
     <section className="home__container">
       <div className="home__cards__container">
-
+        {products.map((product) => (
+          <ProductCard key={product.id} className="home__card">
+            <img
+              className="home__card__img"
+              src={product.href}
+              alt={product.nombre}
+            />
+            <p className="home__card__paragraph">{product.nombre}</p>
+            <p className="home__card__paragraph">
+              {product.precio.toLocaleString("es-CL", {
+                style: "currency",
+                currency: "CLP",
+              })}
+            </p>
+          </ProductCard>
+        ))}
       </div>
     </section>
   );
