ccb716a274272fa467dcd4bcdff8bfc4c8f4a067
Edison <aalevenegass@gmail.com>
2024-06-12 11:57:53 -0400

    Creando componente carrito modal

---
+++ Frontend/src/components/carritoModal/CarritoModal.jsx
@@ -0,0 +1,9 @@
+import "../carritoModal/carritoModal.css"
+
+export function CarritoModal() {
+  return (
+    <section className="carritomodal__container">
+      <h1>Hola soy el carrito modal jeje komotas</h1>
+    </section>
+  );
+}
