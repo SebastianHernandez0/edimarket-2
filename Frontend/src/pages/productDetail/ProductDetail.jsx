63d72f66dff76d7a44739c19eaae93065c313801
Edison <aalevenegass@gmail.com>
2024-06-12 10:57:55 -0400

    Agregando ícono de likes para favoritos

--- Frontend/src/pages/productDetail/ProductDetail.jsx
+++ Frontend/src/pages/productDetail/ProductDetail.jsx
@@ -3,6 +3,8 @@ import { useContext } from "react";
 import { ProductContext } from "../../context/ProductContext";
 import { ProductCard } from "../../components/productCard/ProductCard";
 import { GeneralBtn } from "../../components/generalBtn/GeneralBtn";
+import { IoHeartSharp } from "react-icons/io5";
+import { GoHeart } from "react-icons/go";
 
 export function ProductDetail() {
   const { productById } = useContext(ProductContext);
@@ -17,12 +19,20 @@ export function ProductDetail() {
               <p className="card__paragraph card__paragraph__name">
                 {productById?.nombre}
               </p>
-              <p className="card__paragraph card__paragraph__price">
-                {productById?.precio.toLocaleString("es-CL", {
-                  style: "currency",
-                  currency: "CLP",
-                })}
-              </p>
+              <div className="card__info__price__details">
+                <p className="card__paragraph card__paragraph__price">
+                  {productById?.precio.toLocaleString("es-CL", {
+                    style: "currency",
+                    currency: "CLP",
+                  })}
+                </p>
+                {productById.like ? (
+                  <IoHeartSharp className="card__info__like__icon" />
+                ) : (
+                  <GoHeart className="card__info__like__icon" />
+                )}
+              </div>
+
               <p className="card__paragraph card__paragraph__stock">
                 Stock disponible{" "}
                 <span className="font-semibold">{productById?.stock}</span>
