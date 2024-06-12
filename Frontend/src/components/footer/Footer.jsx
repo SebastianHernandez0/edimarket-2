5b9a0e692dbdec6acfdc11f435a0279571bfc4c0
Edison <aalevenegass@gmail.com>
2024-06-12 11:49:35 -0400

    Creando y estilando footer

--- Frontend/src/components/footer/Footer.jsx
+++ Frontend/src/components/footer/Footer.jsx
@@ -1,9 +1,67 @@
 import "../footer/footer.css";
+import { FaFacebookSquare } from "react-icons/fa";
+import { FaInstagram } from "react-icons/fa";
+import { FaYoutube } from "react-icons/fa";
+import { Link } from "react-router-dom";
 
 export function Footer() {
   return (
     <footer className="footer__container">
-      <h1 className="footer__title">Hola soy el footer</h1>
+      <div className="footer__subcontainer">
+        <div className="footer__logo">
+          <img
+            className="footer__logo__img"
+            src="./imgs/aplication/logo2.png"
+            alt="logo"
+          />
+          <p className="footer__logo__text">
+            Todo lo que necesitas en un mismo lugar
+          </p>
+        </div>
+        <div className="footer__section">
+          <h4 className="footer__section__title">Ayuda</h4>
+          <div className="footer__section__list">
+            <Link className="footer__section__link">Centro de ayuda</Link>
+            <Link className="footer__section__link">
+              Formulario de contacto
+            </Link>
+            <Link className="footer__section__link">Reclamos</Link>
+          </div>
+        </div>
+        <div className="footer__section">
+          <h4 className="footer__section__title">Nosotros</h4>
+          <div className="footer__section__list">
+            <Link className="footer__section__link">Quiénes somos</Link>
+            <Link className="footer__section__link">Contacto</Link>
+            <Link className="footer__section__link">Encuéntranos</Link>
+            <Link className="footer__section__link">Tiendas</Link>
+            <Link className="footer__section__link">Blogs</Link>
+          </div>
+        </div>
+        <div className="footer__section">
+          <h4 className="footer__section__title">Servicio al cliente</h4>
+          <div className="footer__section__list">
+            <Link className="footer__section__link">
+              Términos y condiciones
+            </Link>
+            <Link className="footer__section__link">Promociones</Link>
+            <Link className="footer__section__link">Devoluciones</Link>
+            <Link className="footer__section__link">Servicios técnicos</Link>
+            <Link className="footer__section__link">Asistencia de compra</Link>
+          </div>
+        </div>
+        <div className="footer__social">
+          <h4 className="footer__social__title">Social</h4>
+          <div className="footer__social__icons">
+            <FaFacebookSquare className="footer__social__icon" />
+            <FaInstagram className="footer__social__icon" />
+            <FaYoutube className="footer__social__icon" />
+          </div>
+          <div className="footer__content">
+            <span>EDIMARKET | Todos los derechos reservados | 2024</span>
+          </div>
+        </div>
+      </div>
     </footer>
   );
 }
