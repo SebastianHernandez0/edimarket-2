c3d0ea7aa0e6a73478eaefeb30c757e20f08b003
Edison <aalevenegass@gmail.com>
2024-06-12 13:38:22 -0400

    Agregando ícono para cerrar carrito más una función para la misma

--- Frontend/src/components/carritoModal/CarritoModal.jsx
+++ Frontend/src/components/carritoModal/CarritoModal.jsx
@@ -1,13 +1,32 @@
 import { useContext } from "react";
 import "../carritoModal/carritoModal.css";
 import { CartContext } from "../../context/CarritoContext";
+import { IoCloseOutline } from "react-icons/io5";
 
 export function CarritoModal() {
-  const { cartModal } = useContext(CartContext);
+  const { cartModal, setCartModal } = useContext(CartContext);
+
+  const closeCartModal = () => {
+    if (cartModal) {
+      setCartModal(false);
+    }
+  };
 
   return (
-    <section className="carritomodal__container">
-      {cartModal ? <h1>Hola soy el carrito modal jeje komotas</h1> : ""}
+    <section className="cartmodal__container">
+      {cartModal ? (
+        <div className="cartmodal__body rounded-md shadow">
+          <IoCloseOutline
+            onClick={closeCartModal}
+            className="cartmodal__icon"
+          />
+          <h1 className="cartmodal__title text-center font-medium mt-3">
+            Agregaste al carrito
+          </h1>
+        </div>
+      ) : (
+        ""
+      )}
     </section>
   );
 }
