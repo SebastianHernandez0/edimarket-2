6fb813cf2ab9c28d1539533149e6e0b353d067a7
Edison <aalevenegass@gmail.com>
2024-06-11 22:21:36 -0400

    Creando función para capturar el elemento clickeado

--- Frontend/src/components/products/Products.jsx
+++ Frontend/src/components/products/Products.jsx
@@ -1,10 +1,22 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { ProductCard } from "../../components/productCard/ProductCard.jsx";
 import { ProductContext } from "../../context/ProductContext.jsx";
 import "../products/products.css";
+import { useNavigate } from "react-router-dom";
 
 export function Products() {
   const { products } = useContext(ProductContext);
+  const [productById, setProductById] = useState(null);
+
+  const navigate = useNavigate();
+
+  const handleProductDetail = (id) => {
+    const product = products.find((product) => product.id === id);
+    if (product) {
+      setProductById(product);
+      console.log(product);
+    }
+  };
 
   return (
     <div className="products__container">
@@ -17,21 +29,23 @@ export function Products() {
             key={product.id}
             className="products__card shadow-md bg-white"
           >
-            <img
-              className="products__card__img"
-              src={product.href}
-              alt={product.nombre}
-            />
-            <div className="products__card__desc__container px-4">
-              <p className="products__card__paragraph text-slate-700 font-semibold text-lg">
-                {product.nombre}
-              </p>
-              <p className="products__card__paragraph font-semibold text-2xl">
-                {product.precio.toLocaleString("es-CL", {
-                  style: "currency",
-                  currency: "CLP",
-                })}
-              </p>
+            <div onClick={() => handleProductDetail(product.id)}>
+              <img
+                className="products__card__img"
+                src={product.href}
+                alt={product.nombre}
+              />
+              <div className="products__card__desc__container px-4">
+                <p className="products__card__paragraph text-slate-700 font-semibold text-lg">
+                  {product.nombre}
+                </p>
+                <p className="products__card__paragraph font-semibold text-2xl">
+                  {product.precio.toLocaleString("es-CL", {
+                    style: "currency",
+                    currency: "CLP",
+                  })}
+                </p>
+              </div>
             </div>
           </ProductCard>
         ))}
