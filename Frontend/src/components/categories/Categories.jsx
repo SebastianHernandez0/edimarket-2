7fba8bedc3a8f673dafd9781933c5bcf8cf0fa9a
Edison <aalevenegass@gmail.com>
2024-06-13 11:04:14 -0400

    Agregando efecto hover en links de categorías

--- Frontend/src/components/categories/Categories.jsx
+++ Frontend/src/components/categories/Categories.jsx
@@ -7,40 +7,43 @@ export function Categories() {
   const { openCategories } = useContext(ProductContext);
 
   return (
-    <section className="categories__container ">
+    <section className="categories__container">
       {openCategories ? (
-        <div className="categories__list bg-gray-200 shadow-md">
-          <Link to="/category/consolas">
+        <div className="categories__list bg-gray-200 shadow-md ">
+          <Link className="categories__list__link" to="/category/consolas">
             <div>
               <h3>Consolas</h3>
             </div>
           </Link>
-          <Link to="/category/accesorios">
+          <Link className="categories__list__link" to="/category/accesorios">
             <div>
               <h3>Accesorios</h3>
             </div>
           </Link>
-          <Link to="/category/electrodomesticos">
+          <Link
+            className="categories__list__link"
+            to="/category/electrodomesticos"
+          >
             <div>
               <h3>Electrodomésticos</h3>
             </div>
           </Link>
-          <Link to="/category/computadores">
+          <Link className="categories__list__link" to="/category/computadores">
             <div>
               <h3>computadores</h3>
             </div>
           </Link>
-          <Link to="/category/ropa">
+          <Link className="categories__list__link" to="/category/ropa">
             <div>
               <h3>Ropa</h3>
             </div>
           </Link>
-          <Link to="/category/telefonia">
+          <Link className="categories__list__link" to="/category/telefonia">
             <div>
               <h3>Telefonía</h3>
             </div>
           </Link>
-          <Link to="/category/publicaciones">
+          <Link className="categories__list__link" to="/category/publicaciones">
             <div>
               <h3>Publicaciones</h3>
             </div>
