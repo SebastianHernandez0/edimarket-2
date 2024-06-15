fe99157df93297135e1be9e1ee0b7faab6cac580
Edison <aalevenegass@gmail.com>
2024-06-15 02:23:15 -0400

    Instalando slider (dependencia)

--- Frontend/src/components/header/Header.jsx
+++ Frontend/src/components/header/Header.jsx
@@ -1,9 +1,47 @@
 import "../header/header.css";
+import ImageGallery from "react-image-gallery";
+import "react-image-gallery/styles/css/image-gallery.css";
 
 export function Header() {
+  const images = [
+    {
+      original: "/imgs/aplication/slider_1.webp",
+      thumbnail: "/imgs/aplication/slider_1.webp",
+      originalHeight: 300, // Alto en píxeles
+      originalWidth: 400,  // Ancho en píxeles
+    },
+    {
+      original: "/imgs/aplication/slider_2.gif",
+      thumbnail: "/imgs/aplication/slider_2.gif",
+    },
+    {
+      original: "/imgs/aplication/slider_3.jpg",
+      thumbnail: "/imgs/aplication/slider_3.jpg",
+    },
+    {
+      original: "/imgs/aplication/slider_4.png",
+      thumbnail: "/imgs/aplication/slider_4.png",
+    },
+    {
+      original: "/imgs/aplication/slider_5.webp",
+      thumbnail: "/imgs/aplication/slider_5.webp",
+    },
+  ];
+
   return (
     <header className="header__container">
-      <h1 className="">Hola soy el header</h1>
+      <div className="slider__container">
+        <ImageGallery
+          showPlayButton={false}
+          showFullscreenButton={false}
+          showThumbnails={false}
+          items={images}
+          showBullets={true}
+          autoPlay={true}
+          slideInterval={5000}
+          slideDuration={1000}
+        />
+      </div>
     </header>
   );
 }
