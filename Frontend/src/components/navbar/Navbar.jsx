77226f9e19a4960e32895d5928e666e6fc05a953
Edison <aalevenegass@gmail.com>
2024-06-18 14:09:37 -0400

    Agregando overlay a menu hamburguesa

--- Frontend/src/components/navbar/Navbar.jsx
+++ Frontend/src/components/navbar/Navbar.jsx
@@ -14,6 +14,7 @@ import { BurgerCategories } from "../burgerCategories/BurgerCategories.jsx";
 import { CiUser } from "react-icons/ci";
 import { CiHeart } from "react-icons/ci";
 import { GeneralBtn } from "../generalBtn/GeneralBtn.jsx";
+import { OverlayScreen } from "../overlayScreen/OverlayScreen.jsx";
 
 // Crear un componente envolvente para manejar la referencia
 const UserIcon = forwardRef((props, ref) => (
@@ -121,6 +122,7 @@ export function Navbar() {
 
   return (
     <nav ref={navbarRef} className="navbar__container shadow-sm">
+      <OverlayScreen clicked={clicked} />
       <div className="navbar__logo__container">
         <div className="navbar__logo__section">
           <img
--- Frontend/src/components/overlayScreen/OverlayScreen.jsx
+++ Frontend/src/components/overlayScreen/OverlayScreen.jsx
@@ -2,20 +2,20 @@ import { useContext, useEffect, useRef } from "react";
 import "../overlayScreen/overlayScreen.css";
 import { CartContext } from "../../context/CarritoContext";
 
-export function OverlayScreen() {
+export function OverlayScreen({ clicked }) {
   const { cartModal } = useContext(CartContext);
 
   const overlayRef = useRef(null);
 
   useEffect(() => {
-    if (cartModal) {
+    if (cartModal || clicked) {
       overlayRef.current.classList.remove("overlayNone");
       overlayRef.current.classList.add("overlayVisible");
     } else {
       overlayRef.current.classList.add("overlayNone");
       overlayRef.current.classList.remove("overlayVisible");
     }
-  }, [cartModal]);
+  }, [cartModal, clicked]);
 
   return <div ref={overlayRef} className="overlay overlayNone"></div>;
 }
