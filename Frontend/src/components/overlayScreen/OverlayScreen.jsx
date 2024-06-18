75ab94469c299f32c7be93e5ed103ac7105c515b
Edison <aalevenegass@gmail.com>
2024-06-18 15:33:40 -0400

    Ajustando estilo de overlay

--- Frontend/src/components/overlayScreen/OverlayScreen.jsx
+++ Frontend/src/components/overlayScreen/OverlayScreen.jsx
@@ -9,12 +9,18 @@ export function OverlayScreen({ clicked }) {
 
   useEffect(() => {
     if (cartModal || clicked) {
+      document.body.style.overflow = "hidden";
       overlayRef.current.classList.remove("overlayNone");
       overlayRef.current.classList.add("overlayVisible");
     } else {
+      document.body.style.overflow = "auto";
       overlayRef.current.classList.add("overlayNone");
       overlayRef.current.classList.remove("overlayVisible");
     }
+
+    return () => {
+      document.body.style.overflow = "auto";
+    };
   }, [cartModal, clicked]);
 
   return <div ref={overlayRef} className="overlay overlayNone"></div>;
