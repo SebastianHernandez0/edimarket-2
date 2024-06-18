3216be0822995ad5cc93d4ab516f0aa64fe76fd1
Edison <aalevenegass@gmail.com>
2024-06-18 15:57:58 -0400

    Estilando barra de desplazamiento

--- Frontend/src/components/overlayScreen/OverlayScreen.jsx
+++ Frontend/src/components/overlayScreen/OverlayScreen.jsx
@@ -9,17 +9,17 @@ export function OverlayScreen({ clicked }) {
 
   useEffect(() => {
     if (cartModal || clicked) {
-      document.body.style.overflow = "hidden";
+      document.body.style.overflowY = "hidden";
       overlayRef.current.classList.remove("overlayNone");
       overlayRef.current.classList.add("overlayVisible");
     } else {
-      document.body.style.overflow = "auto";
+      document.body.style.overflowY = "auto";
       overlayRef.current.classList.add("overlayNone");
       overlayRef.current.classList.remove("overlayVisible");
     }
 
     return () => {
-      document.body.style.overflow = "auto";
+      document.body.style.overflowY = "auto";
     };
   }, [cartModal, clicked]);
 
