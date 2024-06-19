70cc785b11b11abfb56f034308b3499f6504fbe6
Edison <aalevenegass@gmail.com>
2024-06-19 14:22:04 -0400

    Creando contenido en vista direcciones

--- Frontend/src/pages/userAddress/UserAddress.jsx
+++ Frontend/src/pages/userAddress/UserAddress.jsx
@@ -1,9 +1,40 @@
 import "../userAddress/userAddress.css";
+import { HiHome } from "react-icons/hi2";
+import { HiDotsVertical } from "react-icons/hi";
 
 export function UserAddress() {
   return (
-    <section className="useraddress__container">
-      <h1>Hola soy las direcciones</h1>
+    <section className="useraddress__container bg-white shadow-sm rounded-sm">
+      <h1 className="text-2xl font-semibold mb-5">Direcciones</h1>
+      {/*Acá se mapea ya que podrán haber más de una dirección*/}
+      <div className="useradress__body flex justify-between">
+        <div className="w-full">
+          <hr className="w-full" />
+          <div className="direccion flex flex-col gap-2 my-3">
+            <div className="flex gap-2 items-center font-medium">
+              <HiHome className="text-2xl" />
+              <span>
+                Titan <span>4380</span> departamento 403, piso 4 torre 3
+              </span>
+            </div>
+            <div className="region-comuna pl-8 text-sm">
+              <span className="">Región</span> <span>Metropolitana</span>
+            </div>
+            <span className="usuario pl-8 text-sm">
+              Edison Venegas Espinoza
+            </span>
+          </div>
+        </div>
+        <div>
+          <HiDotsVertical className="text-xl mt-3 cursor-pointer" />
+        </div>
+      </div>
+
+      {/*No incluir el hr en el mapeo*/}
+      <hr />
+      <button className="text-normal justify-self-end self-end mt-3">
+        Agregar dirección
+      </button>
     </section>
   );
 }
