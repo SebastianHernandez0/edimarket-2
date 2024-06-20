b44866d66243e7729b40dd83fca91821bf583a6d
Edison <aalevenegass@gmail.com>
2024-06-20 15:39:23 -0400

    Ajustando imágen en notFound

--- Frontend/src/pages/notFound/NotFound.jsx
+++ Frontend/src/pages/notFound/NotFound.jsx
@@ -8,7 +8,7 @@ export function NotFound() {
         <img
           src="/imgs/aplication/404.png"
           alt=""
-          className="w-6/10 sm:w-full"
+          className="w-2/3 sm:w-3/4"
         />
         <h1 className="text-center">Ups... Parece que esta página no existe</h1>
         <Link to="/" className="font-semibold text-teal-500 text-lg">
