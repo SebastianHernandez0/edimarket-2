fdf7c391c08a25d2bfec13be1f9e7e02f83b5513
Sebastián Hernández <sebastian.her0548@gmail.com>
2024-06-22 17:04:04 -0400

    Borrando dependencias innecesarias

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -2,26 +2,15 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const session = require("express-session");
-const bodyParser = require("body-parser");
 const userRoutes= require("./routes/userRoutes");
 const categoriaRoutes= require("./routes/categoriaRoutes");
 const productRoutes= require("./routes/productRoutes");
 const favoritosRoutes= require("./routes/favoritosRoutes");
 const carritoRoutes= require("./routes/carritoRoutes");
 
-app.use(session({
-  secret: process.env.SESSION_SECRET,
-  resave: false,
-  saveUninitialized: false,
-  cookie: {
-    maxAge: 1000 * 60 * 60 * 24 * 30,
-  },
-}));
-
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+}); 
 
 app.use(cors());
 app.use(express.json());
@@ -41,6 +30,7 @@ app.get("*", (req, res) => {
 
 
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
-});
+
+
+
+module.exports = app;
