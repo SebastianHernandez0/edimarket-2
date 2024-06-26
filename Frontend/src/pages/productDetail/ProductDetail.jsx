73d33122eb9225bbcd965015c505b8500f2b06e4
Edison <aalevenegass@gmail.com>
2024-06-26 14:20:44 -0400

    Ajustando estilos de alerta vista libre

--- Frontend/src/pages/productDetail/ProductDetail.jsx
+++ Frontend/src/pages/productDetail/ProductDetail.jsx
@@ -379,13 +379,13 @@ export function ProductDetail() {
                 </div>
                 <div className="flex gap-5 items-center justify-center mt-5 ">
                   <Link
-                    className="font-semibold sm:text-sm hover:text-teal-400"
+                    className="font-bold sm:text-sm bg-gray-200 w-2/5 py-2 px-4 hover:brightness-75 rounded-md text-gray-800"
                     to="/sign-in"
                   >
                     INGRESAR
                   </Link>
                   <Link
-                    className="font-semibold sm:text-sm hover:text-teal-400"
+                    className="font-bold sm:text-sm bg-gray-200 w-2/5 py-2 px-4 hover:brightness-75 rounded-md text-gray-800"
                     to="/sign-up"
                   >
                     REGISTRARSE
