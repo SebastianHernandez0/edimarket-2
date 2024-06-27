5884ee7e2fb7ed49756228c3e52b82576b716ad9
Sebastián Hernández <sebastian.her0548@gmail.com>
2024-06-27 08:01:23 -0400

    Update database.js

--- Backend/src/config/database.js
+++ Backend/src/config/database.js
@@ -1,6 +1,8 @@
 const {Pool} = require('pg');
 
 const pool = new Pool({
+    connectionString: process.env.DB_CONNECTION,
+    ssl:true,
     user: process.env.DB_USER,
     host: process.env.DB_HOST,
     database: process.env.DB_DB,
