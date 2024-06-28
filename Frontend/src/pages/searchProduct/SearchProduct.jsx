7607db6bd3c7e667388a47ea532802ac34b40778
Edison <aalevenegass@gmail.com>
2024-06-28 17:33:02 -0400

    Añadiendo feedback al no econtrar resultados en la búsqueda

--- Frontend/src/pages/searchProduct/SearchProduct.jsx
+++ Frontend/src/pages/searchProduct/SearchProduct.jsx
@@ -4,14 +4,20 @@ import { ProductContext } from "../../context/ProductContext";
 import { ProductCard } from "../../components/productCard/ProductCard";
 
 export function SearchProduct() {
-  const { findedProduct, handleProductDetail } = useContext(ProductContext);
+  const { findedProduct, handleProductDetail, searchProduct } =
+    useContext(ProductContext);
 
   return (
     <section className="searchproduct__container">
-      <h1>Resultados de la búsqueda</h1>
+      <h1 className="products__title text-2xl font-medium">
+        Resultados de la búsqueda
+      </h1>
+      <span className="mt-2 text-sm font-medium text-slate-700">
+        Palabra clave : <span>{searchProduct}</span>{" "}
+      </span>
       <div className="products__cards__container">
         {findedProduct.length > 0 ? (
-          findedProduct.map((product) => (
+          findedProduct?.map((product) => (
             <ProductCard
               onClick={() => handleProductDetail(product?.id)}
               key={product?.id}
@@ -40,6 +46,12 @@ export function SearchProduct() {
         ) : (
           <div>
             <h1>No hay resultados para tu búsqueda</h1>
+            <div className="flex flex-col mt-5">
+              <ul className="flex flex-col gap-1">
+                <li>Revisa la ortografía de lo que escribiste.</li>
+                <li>Utiliza el nombre del producto que deseas buscar.</li>
+              </ul>
+            </div>
           </div>
         )}
       </div>
