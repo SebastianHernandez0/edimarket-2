4cd816bda6a870d553adec2f5a8410df6cb22b27
Edison <aalevenegass@gmail.com>
2024-06-29 17:46:05 -0400

    Creando componente para la paginación

---
+++ Frontend/src/components/pagination/Pagination.jsx
@@ -0,0 +1,9 @@
+import "../pagination/pagination.css";
+
+export function Pagination() {
+  return (
+    <section className="pagination__container">
+      <h1>Hola soy la paginación</h1>
+    </section>
+  );
+}
