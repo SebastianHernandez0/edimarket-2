ae482382f52ac3e4cb6cb08690c347e3845b5998
Edison <aalevenegass@gmail.com>
2024-07-04 13:20:05 -0400

    Cambiando estilo del carro vacío

--- Frontend/src/components/emptyCart/EmptyCart.jsx
+++ Frontend/src/components/emptyCart/EmptyCart.jsx
@@ -1,18 +1,24 @@
-import React from "react";
-import emptyCart from "/public/imgs/aplication/emptycart.png";
+import emptyCart from "/imgs/aplication/empty-cart.png";
 import { GeneralBtn } from "../generalBtn/GeneralBtn";
 import { NavLink } from "react-router-dom";
 import "../emptyCart/emptyCart.css";
 
 export function EmptyCart() {
   return (
-    <div className="emptyCart__container flex justify-center items-center flex-col mb-50">
-      <img src={emptyCart} alt="empty cart" className="my-[20px] h-96" />
-      <h1 className="text-center">Tu carro está vacío</h1>
-      <p className="pb-[50px]">Suma productos y consigue envío gratis :D</p>
-      <GeneralBtn type="primary">
-        <NavLink to="/">Volver a productos</NavLink>
-      </GeneralBtn>
+    <div className="emptyCart__container flex justify-center items-center flex-col mb-50 bg-gray-50 shadow-md rounded">
+      <div className="emptyCart__body flex flex-col items-center h-[500px] justify-center">
+        <div className="flex flex-col gap-3 items-center">
+          <img src={emptyCart} alt="empty cart" className="w-[200px] sm:w-[250px]" />
+          <h1 className="text-center">¡Tu carro está vacío!</h1>
+          <p className="pb-[50px]">
+            Añade algunos productos para empezar a comprar
+          </p>
+        </div>
+
+        <GeneralBtn type="primary">
+          <NavLink to="/">Volver a productos</NavLink>
+        </GeneralBtn>
+      </div>
     </div>
   );
 }
