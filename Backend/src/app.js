31e9b7ecf1b0fa7aae815baa0fc576a3c649b52d
Edison <aalevenegass@gmail.com>
2024-07-03 22:30:02 -0400

    Quitando registro con google (momentáneo)

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -9,9 +9,6 @@ const favoritosRoutes = require("./routes/favoritosRoutes");
 const carritoRoutes = require("./routes/carritoRoutes");
 const ventaRoutes = require("./routes/ventaRoutes");
 const port = process.env.PORT || 3000;
-const session = require("express-session");
-const passport = require("./controllers/passportConfig");
-const authRoutes = require("./routes/authRoutes");
 
 app.listen(port, () => {
   console.log(`Servidor escuchando en http://localhost:${port}`);
@@ -19,15 +16,6 @@ app.listen(port, () => {
 
 app.use(cors());
 app.use(express.json());
-app.use(
-  session({
-    secret: process.env.SESSION_SECRET,
-    resave: false,
-    saveUninitialized: true,
-  })
-);
-app.use(passport.initialize());
-app.use(passport.session());
 
 app.use("/usuarios", userRoutes);
 app.use("/categorias", categoriaRoutes);
@@ -35,7 +23,6 @@ app.use("/productos", productRoutes);
 app.use("/favoritos", favoritosRoutes);
 app.use("/carrito", carritoRoutes);
 app.use("/venta", ventaRoutes);
-app.use("/auth", authRoutes);
 
 app.get("*", (_, res) => {
   res.status(404).send("No encontrado");
--- Backend/src/config/database.js
+++ Backend/src/config/database.js
@@ -1,14 +1,12 @@
-const {Pool} = require('pg');
+const { Pool } = require("pg");
 
 const pool = new Pool({
-    connectionString: process.env.DB_CONNECTION,
-    ssl:true,
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database: process.env.DB_DB,
-    password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT,
-    allowExitOnIdle: true,
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_DB,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
+  allowExitOnIdle: true,
 });
 
 module.exports = pool;
