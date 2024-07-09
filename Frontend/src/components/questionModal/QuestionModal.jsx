4f065850e9700bf55d2fbd8e2c91a230182d7372
Edison <aalevenegass@gmail.com>
2024-07-09 16:45:13 -0400

    Añadiendo renderizado condicional dependiendo de si hay o no preguntas

--- Frontend/src/components/questionModal/QuestionModal.jsx
+++ Frontend/src/components/questionModal/QuestionModal.jsx
@@ -1,17 +1,118 @@
-import { forwardRef } from "react";
+import { forwardRef, useContext, useRef, useState } from "react";
+import { UserContext } from "../../context/UserContext";
+import { ConfirmDelete } from "../confirmDelete/ConfirmDelete";
+import { IoIosClose } from "react-icons/io";
+import { GeneralBtn } from "../generalBtn/GeneralBtn";
+import { ProductContext } from "../../context/ProductContext";
 
-export const QuestionModal = forwardRef((props, ref) => {
+const ModalIcon = forwardRef((props, ref) => (
+  <div ref={ref}>
+    <IoIosClose {...props} />
+  </div>
+));
+
+export const QuestionModal = forwardRef(({ productId, setProductId }, ref) => {
+  const { userToken, user } = useContext(UserContext);
+  const { setLoading, loading } = useContext(ProductContext);
+  const modalRef = useRef(null);
+  const iconRef = useRef(null);
+  const [confirmDeleteId, setConfirmDeleteId] = useState("");
+
+  const handleDeleteQuestions = async () => {
+    try {
+      const response = await fetch(
+        `http://localhost:3000/usuarios/preguntas/${productId}`,
+        {
+          method: "DELETE",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${userToken}`,
+          },
+          body: JSON.stringify({ id: user.id }),
+        }
+      );
+
+      if (!response.ok) {
+        const errorData = await response.json();
+        throw new Error(errorData.message || "Error al eliminar preguntas");
+      }
+
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      console.error("Error:", error.message);
+      throw error;
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const handleConfirmDeleteQuestions = (productId) => {
+    if (productId) {
+      handleDeleteQuestions();
+    }
+  };
+
+  const handleOpenConfirmModal = (id) => {
+    if (id) {
+      setConfirmDeleteId(id);
+    }
+  };
+
   return (
     <div
       ref={ref}
       className="questionmodal__container absolute top-[50px] right-[20px] border flex justify-center flex-col gap-2 z-50 rounded-md shadow bg-gray-100 overflow-hidden"
     >
-      <p className="hover:bg-teal-200 rounded-sm cursor-pointer py-2 px-3 text-sm font-medium select-none">
+      <p
+        onClick={() => handleOpenConfirmModal(productId)}
+        className="hover:bg-teal-200 rounded-sm cursor-pointer py-2 px-3 text-sm font-medium select-none"
+      >
         Eliminar preguntas
       </p>
       <p className="hover:bg-teal-200 rounded-sm cursor-pointer py-2 px-3 text-sm font-medium select-none">
         Reportar preguntas
       </p>
+      {confirmDeleteId ? (
+        <ConfirmDelete
+          ref={modalRef}
+          className="confirm__delete__modal__card bg-gray-100 shadow-sm p-3 rounded-md flex flex-col items-stretch gap-4 border"
+        >
+          <ModalIcon
+            ref={iconRef}
+            onClick={() => {
+              setProductId("");
+            }}
+            className="close__icon"
+          />
+          <h2 className="text-center">Eliminar preguntas</h2>
+          <hr />
+          <span className="text-center font-medium text-sm">
+            ¿Seguro que quieres eliminar las preguntas?
+          </span>
+          <hr />
+          <div className="flex items-center justify-evenly">
+            <GeneralBtn
+              onClick={() => {
+                setProductId("");
+              }}
+              type="primary"
+              className="confirm__delete__btn"
+            >
+              Cancelar
+            </GeneralBtn>
+            <GeneralBtn
+              onClick={() => handleConfirmDeleteQuestions(productId)}
+              type="primary"
+              className="confirm__delete__btn"
+            >
+              Eliminar
+            </GeneralBtn>
+          </div>
+        </ConfirmDelete>
+      ) : (
+        ""
+      )}
     </div>
   );
 });
--- Frontend/src/pages/myQuestions/MyQuestions.jsx
+++ Frontend/src/pages/myQuestions/MyQuestions.jsx
@@ -21,25 +21,29 @@ export function MyQuestions() {
 
   const handleGetProductWithQuestions = async () => {
     try {
-      const response = await fetch(
-        `http://localhost:3000/productos/producto/${user.id}`,
-        {
-          headers: {
-            "Content-Type": "application/json",
-            Authorization: `Bearer ${userToken}`,
-          },
-        }
-      );
-
-      if (!response.ok) {
-        const errorData = await response.json();
-        throw new Error(
-          errorData.message || "Error al obtener productos con preguntas"
+      if (userToken) {
+        const response = await fetch(
+          `http://localhost:3000/productos/producto/${user.id}`,
+          {
+            headers: {
+              "Content-Type": "application/json",
+              Authorization: `Bearer ${userToken}`,
+            },
+          }
         );
+
+        if (!response.ok) {
+          const errorData = await response.json();
+          throw new Error(
+            errorData.message || "Error al obtener productos con preguntas"
+          );
+        }
+        const data = await response.json();
+        setProduct(data.productos);
+        return data;
+      } else {
+        return;
       }
-      const data = await response.json();
-      setProduct(data.productos);
-      return data;
     } catch (error) {
       console.error("Error:", error.message);
     } finally {
@@ -80,57 +84,69 @@ export function MyQuestions() {
     <section className="myquestions___container bg-white shadow-sm rounded-sm">
       <h1 className="text-2xl font-semibold mb-5">Mis preguntas realizadas</h1>
       <div className="myquestions__body flex flex-col gap-5">
-        {product.map((element) => {
-          return (
-            <div
-              className="border p-4 rounded-md shadow-sm"
-              key={element?.producto_id}
-            >
-              <div className="flex items-center gap-2 relative">
-                <div className="border p-2 rounded-md shadow">
-                  <img
-                    className="w-[80px] object-cover "
-                    src={element?.imagen}
-                    alt=""
-                  />
+        {product.length > 0 ? (
+          product.map((element) => {
+            return (
+              <div
+                className="border p-4 rounded-md shadow-sm"
+                key={element?.producto_id}
+              >
+                <div className="flex items-center gap-2 relative">
+                  <div className="border p-2 rounded-md shadow">
+                    <img
+                      className="w-[80px] object-cover "
+                      src={element?.imagen}
+                      alt=""
+                    />
+                  </div>
+                  <div className="flex items-center justify-between w-full overflow-hidden h-[40px]">
+                    <p className="text-ellipsis overflow-hidden whitespace-nowrap max-w-[150px] md:max-w-full ">
+                      {element?.titulo}
+                    </p>
+                    <DeleteIcon
+                      ref={element?.producto_id === productId ? iconRef : null}
+                      onClick={() => handleOpenModal(element?.producto_id)}
+                      className="cursor-pointer scale-[1.8] select-none hover:bg-slate-200 rounded-full mr-2"
+                    />
+                    {element?.producto_id === productId ? (
+                      <QuestionModal
+                        setProductId={setProductId}
+                        productId={productId}
+                        confirmProductId={element?.producto_id}
+                        ref={modalRef}
+                      />
+                    ) : (
+                      ""
+                    )}
+                  </div>
                 </div>
-                <div className="flex items-center justify-between w-full overflow-hidden h-[40px]">
-                  <p className="text-ellipsis overflow-hidden whitespace-nowrap max-w-[150px] md:max-w-full ">
-                    {element?.titulo}
-                  </p>
-                  <DeleteIcon
-                    ref={element?.producto_id === productId ? iconRef : null}
-                    onClick={() => handleOpenModal(element?.producto_id)}
-                    className="cursor-pointer scale-[1.8] select-none hover:bg-slate-200 rounded-full mr-2"
-                  />
-                  {element?.producto_id === productId ? (
-                    <QuestionModal ref={modalRef} />
-                  ) : (
-                    ""
-                  )}
+                <hr className="my-4" />
+                <div className="flex flex-col gap-2">
+                  {element?.preguntas.map((pregunta) => {
+                    return (
+                      <div
+                        key={pregunta?.id_pregunta}
+                        className="flex items-center gap-3 ml-3 font-medium overflow-hidden"
+                      >
+                        <p className="overflow-hidden text-ellipsis whitespace-nowrap max-w-[180px] sm:max-w-full">
+                          • {pregunta?.pregunta}
+                        </p>
+                        <span className="text-sm text-gray-500">
+                          {pregunta?.fecha}
+                        </span>
+                      </div>
+                    );
+                  })}
                 </div>
               </div>
-              <hr className="my-4" />
-              <div className="flex flex-col gap-2">
-                {element?.preguntas.map((pregunta) => {
-                  return (
-                    <div
-                      key={pregunta?.id_pregunta}
-                      className="flex items-center gap-3 ml-3 font-medium overflow-hidden"
-                    >
-                      <p className="overflow-hidden text-ellipsis whitespace-nowrap max-w-[180px] sm:max-w-full">
-                        • {pregunta?.pregunta}
-                      </p>
-                      <span className="text-sm text-gray-500">
-                        {pregunta?.fecha}
-                      </span>
-                    </div>
-                  );
-                })}
-              </div>
-            </div>
-          );
-        })}
+            );
+          })
+        ) : (
+          <div className="flex flex-col items-center justify-center h-full mt-6">
+            <h3>No has hecho preguntas aún.</h3>
+            <p>Cuando hagas rpeguntas en algún producto aparecerán acá.</p>
+          </div>
+        )}
       </div>
     </section>
   );
