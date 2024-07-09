76121f26d5ad57e2ff3e8d3213f5b655ae40dfeb
Edison <aalevenegass@gmail.com>
2024-07-09 17:27:10 -0400

    Ordenando preguntas por fecha de creación

--- Backend/src/models/productModel.js
+++ Backend/src/models/productModel.js
@@ -136,7 +136,7 @@ const borrarFavorito = async (idFavorito, idUsuario) => {
 const preguntasByProductId = async (idProduct) => {
   const values = [idProduct];
   const query =
-    "SELECT * FROM preguntas_producto WHERE producto_id = $1 ORDER BY id";
+    "SELECT * FROM preguntas_producto WHERE producto_id = $1 ORDER BY fecha DESC";
   const { rows: preguntas } = await db.query(query, values);
   return preguntas;
 };
