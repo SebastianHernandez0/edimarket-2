7707960682925f5b5bb7c87181e9cb414dcabf02
Edison <aalevenegass@gmail.com>
2024-07-10 16:56:06 -0400

    Separando lógica para react slick

--- Frontend/src/components/products/Products.jsx
+++ Frontend/src/components/products/Products.jsx
@@ -9,6 +9,7 @@ import star from "/imgs/aplication/estrella.png";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Slider from "react-slick";
+import { settings } from "../reactslick/ReactSlickSlider.jsx";
 
 export function Products() {
   const { products, handleProductDetail, loading, page } =
@@ -20,41 +21,6 @@ export function Products() {
       window.scrollTo(0, 600);
     }
   }, [page]);
-
-  const settings = {
-    dots: true,
-    infinite: false,
-    speed: 500,
-    slidesToShow: 4,
-    slidesToScroll: 4,
-    initialSlide: 0,
-    responsive: [
-      {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 3,
-          infinite: true,
-          dots: true,
-        },
-      },
-      {
-        breakpoint: 600,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2,
-          initialSlide: 2,
-        },
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
-        },
-      },
-    ],
-  };
 
   return (
     <section>
